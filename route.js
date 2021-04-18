3c8a93c84747aa04faf399b72c893060dd3d9509
Rulla <rulla@Rullas-MacBook-Pro-2.local>
2021-04-18 14:19:53 -0700

    updates

--- route.js
+++ route.js
@@ -1,30 +1,64 @@
 // DEPENDENCIES
 // We need to include the path package to get the correct file path for our html
-const fs=require('fs');
+const fs = require('fs');
 const path = require('path');
 
 
 // ROUTING
 
-module.exports = (app) => {
+module.exports = app => {
   // => HTML GET Requests
   // Below code handles when users "visit" a page.
   // In each of the below cases the user is shown an HTML page of content
-//set up notes to read db.json
-fs.readFile("db/db.json", "utf8", (error, data))=>{
-  if (error) throw error;
-}
+  //set up notes to read db.json
+  fs.readFile("db/db.json", "utf8", (error, data) => {
+    if (error) throw error;
+    let notes = JSON.parse(data);
+  },
   app.get('/api/notes', (req, res) => {
     res.json(notes);
-  });
+  }));
 
 
-  // setup post route
+  // post route
   app.post('/api/notes', (req, res) => {
-const addNotes=req.body;
-notes.oush (addNotes);
-notes.push (addNotes);
-updateDb();
-return console.log ("added");
+    let addNotes = req.body;
+    notes.push(addNotes);
+    updateConnection();
+    return console.log("added");
   });
+
+  // get notes by id
+  app.get("/api/notes/:id", function (req, res) {
+    res.json(notes[req.para.id]);
+
+  });
+
+  // delete notes by id
+  app.delete("/api/notes/:id", function (req, res) {
+    notes.splice(req.params.id, 1);
+    updateConnection();
+  });
+
+  // Show notes.html
+  app.get('/notes', function (req, res) {
+    res.sendFile(path.join(__dirname, "./notes.html"));
+  });
+
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, "./index.html"));
+  });
+
+  // When notes added update the json
+  function updateConnection() {
+    fs.writeFile("db/db.json", JSON.stringify(notes, '/t'), error => {
+      if (error) throw error;
+      return true;
+    });
+  }
+
+
+
 };
+
+
