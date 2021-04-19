e544fa4cfb108cd9d982ba95e4e61dc748bf9aff
Rulla <rulla@Rullas-MacBook-Pro-2.local>
2021-04-19 13:12:56 -0700

    updates to route.js

--- route.js
+++ route.js
@@ -2,51 +2,65 @@
 // We need to include the path package to get the correct file path for our html
 const fs = require('fs');
 const path = require('path');
+const uniqueID = require('uniqid');
 
 
 // ROUTING
 
 module.exports = app => {
+  let notes = require(__dirname + "/./db/db.json");
   // => HTML GET Requests
   // Below code handles when users "visit" a page.
   // In each of the below cases the user is shown an HTML page of content
   //set up notes to read db.json
-  fs.readFile("db/db.json", "utf8", (error, data) => {
-    if (error) throw error;
-    let notes = JSON.parse(data);
-  },
+  // fs.readFile("db/db.json", "utf8", (error, data) => {
+  //   if (error) throw error;
+  //   let notes = JSON.parse(data);
+  // },
   app.get('/api/notes', (req, res) => {
     res.json(notes);
-  }));
+  });
 
 
   // post route
   app.post('/api/notes', (req, res) => {
     let addNotes = req.body;
+    addNotes.id = uniqueID();
     notes.push(addNotes);
-    updateConnection();
-    return console.log("added");
+    // updateConnection();
+    fs.writeFile("db/db.json", JSON.stringify(notes), error => {
+      if (error) throw error;
+
+    });
+    res.end();
   });
 
   // get notes by id
-  app.get("/api/notes/:id", function (req, res) {
-    res.json(notes[req.para.id]);
+  // app.get("/api/notes/:id", function (req, res) {
+  //   res.json(notes[req.para.id]);
 
-  });
+  // });
 
   // delete notes by id
   app.delete("/api/notes/:id", function (req, res) {
-    notes.splice(req.params.id, 1);
-    updateConnection();
+    let filter=notes.filter(function(note){
+return note.id !=req.params.id;
+    })
+    noteData=JSON.stringify(filter)
+    notes=filter;
+    fs.writeFileSync(__dirname+"/./db/db.json",noteData, function(error){
+if (error) throw error;
+    })
+    res.end();
   });
 
   // Show notes.html
   app.get('/notes', function (req, res) {
-    res.sendFile(path.join(__dirname, "./notes.html"));
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
   });
-
+  // catch all 
   app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, "./index.html"));
+    res.sendFile(path.join(__dirname, "./public/index.html"));
   });
 
   // When notes added update the json
--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 // Dependencies
 
 const express = require('express');
-const path = require('path');
-const fs = require("fs");
+
 
 // Sets up the Express App
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT||3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(express.static("public"));
 // Require routes file
 require('./route')(app);
 
